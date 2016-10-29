7a9d46ed20ccc6270e1d8f80101461f284d33e49
muhammadmasud <jamdmasud@yahoo.com>
2016-10-30 01:45:24 +0600

    Add project files.

---
+++ AirBusTicketAutomation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirBusTicketAutomation.Startup))]
+namespace AirBusTicketAutomation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
